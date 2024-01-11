4f6b88b00550fcf64e49638833341286ae0afe76
igor.stojanovic <i_gi95@live.com>
2024-01-11 21:43:33 +0100

    trigger CI/CD

--- PizzaMaster.MSTest/HomeControllerTests.cs
+++ PizzaMaster.MSTest/HomeControllerTests.cs
@@ -28,6 +28,72 @@ namespace MSTest
             _service = new Mock<IHomeService>();
             _controller = new HomeController(_service.Object);
         }
+		
+		  // [TestMethod]
+        // public void TestMethod1()
+        // {
+            // var expectedResponse = new ServiceResponse<Document_ResponseDTO>
+            // {
+                // Errors = new List<string>(),
+                // Success = true,
+                // Payload = new Document_ResponseDTO
+                // {
+                    // company = new Company_ResponseDTO
+                    // {
+                        // RegistrationNumber = "019019019",
+                        // CompanyType = "large"
+                    // },
+                    // data = JObject.Parse(@"
+                    // {
+                      // ""account_number"": ""cea00b23d30f77f27b1ddbcb89b25fb13dbea652a5f6fb6fe9a6bac4ddc5fb0d"",
+                      // ""balance"": 42331.12,
+                      // ""currency"": ""#####"",
+                      // ""transactions"": [
+                        // {
+                          // ""transaction_id"": ""05b68f8f80d45137ec15579cdf91c560b0acaf4ad71a7d5a5352bcee36a3bb81"",
+                          // ""amount"": ""#####"",
+                          // ""date"": ""#####"",
+                          // ""description"": ""#####"",
+                          // ""category"": ""#####""
+                        // },
+                        // {
+                          // ""transaction_id"": ""af8637ccf1fe693c254b44327672cf7589e3c8556de8ccceffd368c2f313e2ee"",
+                          // ""amount"": ""#####"",
+                          // ""date"": ""#####"",
+                          // ""description"": ""#####"",
+                          // ""category"": ""#####""
+                        // },
+                        // {
+                          // ""transaction_id"": ""a183251a8414fd819da06488541d523365d51a7d0d942e272c2dce108f6dcdae"",
+                          // ""amount"": ""#####"",
+                          // ""date"": ""#####"",
+                          // ""description"": ""#####"",
+                          // ""category"": ""#####""
+                        // }
+                      // ]
+                    // }")
+                // }
+            // };
+
+            // var request = new Document_RequestDTO() { DocumentId = Guid.NewGuid(), ProductCode = "ProductA", TenantId = Guid.Parse("3FA85F64-5717-4562-B3FC-2C963F66AFA6") };
+
+            // _service.Setup(uow => uow.GetTaxResponse(request)).Returns(expectedResponse);
+
+
+            // var actionResult = _controller.FinancialRequest(request);
+
+
+            // var result = actionResult.Result as OkObjectResult;
+            // Assert.IsInstanceOfType(result, typeof(OkObjectResult));
+
+
+            // var serizalizedExpectedResponse = JsonConvert.SerializeObject(expectedResponse.Payload,Formatting.Indented);
+            // var serizalizedActualResponse = result.Value.ToString();
+
+            // Assert.AreEqual(serizalizedExpectedResponse, serizalizedActualResponse);
+
+
+        // }
 
         [TestMethod]
         public async Task Index_ReturnsViewWithListOfGlossaryModels()
